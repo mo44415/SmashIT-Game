1c18ea91623051b337c5c75286ef410687f95af1
Jakub Dabrowski <dj44303@zut.edu.pl>
2020-10-26 13:56:00 +0100

    Import Main Character, Test Animator Controller (Jungle 6)

---
+++ SmashIT/Assets/Characters/DogKnight/Scripts/Unit.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    protected CharacterController control;
+    protected Vector3 move = Vector3.zero;
+
+    public float moveSpeed = 3f;
+    public float turnSpeed = 90f;
+    public float jumpSpeed = 5f;
+
+    protected bool jump;
+
+    protected Vector3 gravity = Vector3.zero;
+
+    public virtual void Start()
+    {
+        control = GetComponent<CharacterController>();
+
+        if(!control){
+            Debug.LogError("Unit.Start() " + name + " has no CharacterController!");
+            enabled = false;
+        }
+    }
+
+    public virtual void Update()
+    {
+        move *= moveSpeed;
+
+        if(!control.isGrounded){
+            gravity += Physics.gravity * Time.deltaTime;
+        }
+        else{
+            gravity = Vector3.zero;
+
+            if(jump){
+                gravity.y = jumpSpeed;
+                jump = false;
+            }
+        }
+
+        //move += gravity;
+        control.Move(move * Time.deltaTime);
+    }
+}
