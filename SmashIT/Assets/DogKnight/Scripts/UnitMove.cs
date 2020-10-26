aab3817d190a2f1a48d43516e99028f26ee8e1b4
Jakub Dabrowski <dj44303@zut.edu.pl>
2020-10-26 14:26:19 +0100

    Added controller and scripts

---
+++ SmashIT/Assets/DogKnight/Scripts/UnitMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMove : Unit
+{
+    float cameraRotX = 0f;
+    public float cameraPitchMax = 45f;
+    Animator m_Animator;
+    
+    public override void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        //transform.Rotate(0f, Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime, 0f);
+
+        cameraRotX -= Input.GetAxis("Mouse Y");
+        cameraRotX = Mathf.Clamp(cameraRotX, -cameraPitchMax, cameraPitchMax);
+
+        Camera.main.transform.forward = transform.forward;
+        //Camera.main.transform.Rotate(cameraRotX, 0f, 0f);
+
+        move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        bool hasHorizontalInput = !Mathf.Approximately(Input.GetAxis("Horizontal"), 0f);
+        bool hasVerticalInput = !Mathf.Approximately(Input.GetAxis("Vertical"), 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("isWalking", isWalking);
+
+        move.Normalize();
+
+        move = transform.TransformDirection(move);
+
+        if(Input.GetKeyDown(KeyCode.Space) && control.isGrounded){
+            jump = true;
+        }
+        base.Update();
+    }
+}
