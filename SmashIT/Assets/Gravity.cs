75068ba1343ba390ef6ed9638b6e0c79f7ea60af
mo44415 <mo44415@zut.edu.pl>
2020-11-21 17:15:29 +0100

    UPDATE

---
+++ SmashIT/Assets/Gravity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterPhysics : MonoBehaviour
+{
+
+    Vector3 moveVector;
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+    void Update()
+    {
+
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded == false)
+        {
+            moveVector += Physics.gravity;
+        }
+
+        controller.Move(moveVector * Time.deltaTime);
+
+
+    }
+
+}
